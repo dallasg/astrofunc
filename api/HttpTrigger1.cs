a7ef9b3dce3e246d7de8fae4533d02b1d3c1e825
Dallas Gaddis <dallas@effectivecurrent.com>
2025-05-03 19:59:38 -0500

    On-behalf-of flow

--- api/HttpTrigger1.cs
+++ api/HttpTrigger1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Text.Json;
 using Azure.Identity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -6,56 +8,43 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Graph;
 using Microsoft.Identity.Client;
 
-namespace Company.Function
+namespace Company.Function;
+
+public class HttpTrigger1
 {
-    public class HttpTrigger1
+    private readonly ILogger<HttpTrigger1> _logger;
+
+    public HttpTrigger1(ILogger<HttpTrigger1> logger)
     {
-        private readonly ILogger<HttpTrigger1> _logger;
+        _logger = logger;
+    }
 
-        public HttpTrigger1(ILogger<HttpTrigger1> logger)
-        {
-            _logger = logger;
-        }
+    [Function("getme")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-        [Function("getme")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+        string clientId = Environment.GetEnvironmentVariable("CLIENT_ID")!;
+        string clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET")!;
+        string tenantId = Environment.GetEnvironmentVariable("TENANT_ID")!;
 
-            string userAccessToken = req.Headers["Authorization"].ToString().Replace("Bearer ", "");
+        // 1. Extract ID token from incoming headers
+        var userToken = req.Headers["X-MS-TOKEN-AAD-ID-TOKEN"].FirstOrDefault();
+        if (string.IsNullOrEmpty(userToken))
+            return new UnauthorizedResult();
 
-            string clientId = Environment.GetEnvironmentVariable("CLIENT_ID")!;
-            string clientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET")!;
-            string tenantId = Environment.GetEnvironmentVariable("TENANT_ID")!;
+        var credential = new OnBehalfOfCredential(
+            tenantId,
+            clientId,
+            clientSecret,
+            userToken);
 
-            var scopes = new[] { "User.Read" };
-            var oboToken = req.Headers.Authorization.ToString().Replace("Bearer ", "");
+        // 3. Create Graph client with required scopes
+        var graphClient = new GraphServiceClient(credential, new[] { "https://graph.microsoft.com/.default" });
 
-            return new OkObjectResult(oboToken);
+        // 4. Call Microsoft Graph
+        var me = await graphClient.Me.GetAsync();
 
-            // var cca = ConfidentialClientApplicationBuilder
-            //     .Create(clientId)
-            //     .WithClientSecret(clientSecret)
-            //     .WithAuthority(new Uri($"https://login.microsoftonline.com/{tenantId}"))
-            //     .Build();
-
-            // var authorizationCode = "AUTH_CODE_FROM_REDIRECT";
-
-            // // using Azure.Identity;
-            // var options = new AuthorizationCodeCredentialOptions
-            // {
-            //     AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
-            // };
-
-            // // https://learn.microsoft.com/dotnet/api/azure.identity.authorizationcodecredential
-            // var authCodeCredential = new AuthorizationCodeCredential(
-            //     tenantId, clientId, clientSecret, authorizationCode, options);
-
-            // var graphClient = new GraphServiceClient(authCodeCredential, scopes);
-
-            // var me = await graphClient.Me.GetAsync();
-
-            // return new OkObjectResult(me);
-        }
+        return new OkObjectResult(me);
     }
 }
