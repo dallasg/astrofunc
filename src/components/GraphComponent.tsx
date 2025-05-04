fd2a0d503edec1eb67c00b09b8050949580080d7
Dallas Gaddis <dallas@effectivecurrent.com>
2025-05-03 19:21:58 -0500

    Add all files

--- src/components/GraphComponent.tsx
+++ src/components/GraphComponent.tsx
@@ -4,11 +4,20 @@ import { msalConfig } from '../msalConfig';
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
-export default function GraphComponent() {
+export default async function GraphComponent() {
   const [user, setUser] = useState("");
 
+  const msalInstance = new PublicClientApplication(msalConfig);
+  const result = await msalInstance.loginPopup({ scopes: ["User.Read"] });
+  const accessToken = (await msalInstance.acquireTokenSilent({
+    scopes: ["User.Read"],
+    account: result.account,
+  })).accessToken;
+
   useEffect(() => {
-    fetch('/api/getme')
+    fetch('/api/getme', {headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }})
       .then(res => res.text())
       .then(data => setUser(data));
   }, []);
